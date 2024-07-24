bb1a6513ea2a6be1fcdfb059d8a374b4e568b84c
Kader Bozkaya <87239102+kaderbozkaya@users.noreply.github.com>
2024-07-24 17:04:34 +0300

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useCallback } from "react";
-import { useDropzone } from "react-dropzone";
+import { useDropzone } from "react-dropzone"; //dosya yükleme işlevselliği için dropzone kütüphanesi eklendi
 import "./App.css";
 import { FaUpload } from "react-icons/fa";
 import logo from "./assets/unnamed.png";
@@ -11,6 +11,7 @@ const App = () => {
   const originalWidthToHeightRatio = useRef(1);
   const canvasRef = useRef(null);
 
+  // onDrop işlevi, dosya bırakma işlemi gerçekleştiğinde çağrılıyor.
   const onDrop = useCallback((acceptedFiles) => {
     const file = acceptedFiles[0];
     if (file) {
@@ -22,35 +23,29 @@ const App = () => {
 
   const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
-  const handleFileChange = (event) => {
-    const file = event.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = () => openImage(reader.result);
-      reader.readAsDataURL(file);
-    }
-  };
-
+  //openImage bir görüntüyü yükleyip boyutlarını ayarlıyor ve resizeImage işlevini çağırıyor
   const openImage = (src) => {
     const img = new Image();
     img.onload = () => {
       originalWidthToHeightRatio.current = img.width / img.height;
+      setImage(img);
       setWidth(img.width);
       setHeight(img.height);
       resizeImage(img.width, img.height);
     };
     img.src = src;
-    setImage(img);
   };
-
+  //resizeImage görüntüyü belirli boyutlarda yeniden çizmek için kullanılıyor
   const resizeImage = (newWidth, newHeight) => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
     canvas.width = newWidth;
     canvas.height = newHeight;
-    ctx.drawImage(image, 0, 0, newWidth, newHeight);
+    if (image) {
+      ctx.drawImage(image, 0, 0, newWidth, newHeight);
+    }
   };
-
+  //handleWidthChange ve handleHeightChange kullanıcı genişlik veya yükseklik girdiğinde çağrılıyor
   const handleWidthChange = (event) => {
     const newWidth = Number(event.target.value);
     setWidth(newWidth);
@@ -63,6 +58,7 @@ const App = () => {
     resizeImage(width, newHeight);
   };
 
+  //downloadImage işlevi, görselin eklenip eklenmediğini kontrol ediyor boşşa uyarı veriyor indirmiyor
   const downloadImage = () => {
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
@@ -73,9 +69,9 @@ const App = () => {
       canvas.height
     ).data;
 
-    const isEmpty = canvasData.every((pixel, index) => {
-      return index % 4 === 3 ? pixel === 0 : true;
-    });
+    const isEmpty = Array.from(canvasData).every((pixel, index) =>
+      index % 4 === 3 ? pixel === 0 : true
+    );
 
     if (isEmpty) {
       alert("Canvas is empty. No image to download.");
@@ -92,13 +88,13 @@ const App = () => {
     <>
       <div className="header">
         <div className="logo">
-          <img src={logo} alt="" className="logo-image" />
+          <img src={logo} alt="Logo" className="logo-image" />
           <p>Canvas Image Resizer</p>
         </div>
         <div className="links">
-          <a href="">Blog</a>
-          <a href="">How it works?</a>
-          <a href="">Contact Us</a>
+          <a href="#">Blog</a>
+          <a href="#">How it works?</a>
+          <a href="#">Contact Us</a>
         </div>
       </div>
       <h1>Resize an image</h1>
@@ -107,40 +103,43 @@ const App = () => {
         Change image dimensions in bulk.
       </p>
       <div className="resizer">
-        {/* <input
-          type="file"
-          className="resizer__file"
-          onChange={handleFileChange}
-        /> */}
-
         <div {...getRootProps({ className: "dropzone" })}>
           <input {...getInputProps()} />
-          {!image && <FaUpload className="upload-icon" />}
-          <p>Drop your images here or browse.</p>
+          {!image && (
+            <>
+              <FaUpload className="upload-icon" />
+              <p>Drop your images here or browse.</p>
+            </>
+          )}
         </div>
-        {/* {!image && <FaUpload className="upload-icon" />} */}
+
         <div className="image-and-btn">
           <canvas ref={canvasRef} className="canvas"></canvas>
         </div>
       </div>
-      <div className="dimensions">
-        <input
-          type="number"
-          className="resizer__input resizer__input--width"
-          value={width}
-          onChange={handleWidthChange}
-        />
-        x
-        <input
-          type="number"
-          className="resizer__input resizer__input--height"
-          value={height}
-          onChange={handleHeightChange}
-        />
-      </div>
-      <button onClick={downloadImage} className="btn">
-        Download Image
-      </button>
+      {image && (
+        <>
+          <div className="dimensions">
+            <h2>Choose new size</h2>
+            <input
+              type="number"
+              className="resizer-input"
+              value={width}
+              onChange={handleWidthChange}
+            />
+            x
+            <input
+              type="number"
+              className="resizer-input"
+              value={height}
+              onChange={handleHeightChange}
+            />
+          </div>
+          <button onClick={downloadImage} className="btn">
+            Download Image
+          </button>
+        </>
+      )}
     </>
   );
 };
